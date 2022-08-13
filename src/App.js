7488d7dbebe3f1a0fe6b336133eef4493f1d5862
MADAO <56245152+MADAOU@users.noreply.github.com>
2022-08-14 02:57:25 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Switch, Route, } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Top from './pages/top';
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <>
-      <BrowserRouter>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact={true} path='/' component={Top} />
-          <Route path='/index' component={Top} />
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
-      </BrowserRouter>
-    </>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { BrowserRouter as Router } from "react-router-dom";
+
+render(
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
