ac4b64a3854e4f54e20fc040d3992d959b6edce5
RyuSeiri <56245152+RyuSeiri@users.noreply.github.com>
2026-04-16 12:41:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter, Switch, Route, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Top from './pages/top';
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Switch>
-          <Route exact={true} path='/gluttonous_snake' component={Top} />
-          <Route path='/index' component={Top} />
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Top />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
